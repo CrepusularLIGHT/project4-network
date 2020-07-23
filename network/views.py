e5a6468aa1df945032e6625e45841e6b7d056429
LIGHT <64881003+CrepusularLIGHT@users.noreply.github.com>
2020-07-23 13:29:07 -0600

    version 2

--- network/views.py
+++ network/views.py
@@ -1,14 +1,50 @@
+import datetime
+
+from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
+
+# Form used to create NEW POST
+class PostForm(forms.Form):
+    content = forms.CharField(label="", widget=forms.Textarea(attrs={
+        'id': 'new-post-textarea',
+        'class': 'form-control',
+        'name': 'new-post-textarea',
+        'cols': 10,
+        'rows': 3
+    }))
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == "POST":
+        # New post 
+        new_post_form = PostForm(request.POST) #Gets info from form
+        # Check if valid
+        if new_post_form.is_valid():
+            # Get username
+            username = "" ## <<<< THIS IS WHERE I LEFT OFF, get username
+            # Extract content of post
+            content = new_post_form.cleaned_data["content"]
+            # Get current time
+            time = datetime.datetime.now().strftime("%b/%d/%y %I:%M %p")
+            # Number of likes
+            likes = 0
+            # Create the post 
+            new_post = Post(username, content, time, likes)
+            # Print to console
+            print(new_post, new_post.username, new_post.content, new_post.time, new_post.likes)
+            return render(request, "network/index.html", {
+                "postForm": PostForm()
+            })
+
+    return render(request, "network/index.html", {
+        "postForm": PostForm()
+    })
 
 
 def login_view(request):
